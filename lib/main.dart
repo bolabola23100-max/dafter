86f624f2081223508fd8a44508c8c076571d002d
BolaAtef <bolabola23100@gmail.com>
2026-09-06 22:21:57 +0300

    Run automatic daily database backup on startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:dafter/core/backup/backup_service.dart';
+import 'package:dafter/core/database/app_database.dart';
 import 'package:dafter/features/sidebar/screens/sidebar_screen.dart';
 import 'package:flutter/material.dart';
-import 'core/app_theme.dart';
 
-import 'package:dafter/core/database/app_database.dart';
+import 'core/app_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await AppDatabase.instance.database;
+  // If the user has selected a backup folder, keep a daily SQLite backup there.
+  await BackupService.instance.backupDatabase();
 
   runApp(const DafterApp());
 }
