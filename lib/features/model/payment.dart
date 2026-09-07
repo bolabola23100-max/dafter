7e2070b5cef03fce7c89baa6f73da86d1ee63187
BolaAtef <bolabola23100@gmail.com>
2026-09-07 23:22:24 +0300

    Validate payment amounts

--- lib/features/model/payment.dart
+++ lib/features/model/payment.dart
@@ -5,19 +5,11 @@ enum PaymentType {
 
 class Payment {
   final String id;
-
   final PaymentType type;
-
-  /// العميل أو المورد أو أي شخص مرتبط بالدفعة
   final String? personId;
-
-  /// الحساب اللي دخلت/خرجت منه الفلوس
   final String accountId;
-
   final double amount;
-
   final DateTime date;
-
   final String? notes;
 
   Payment({
@@ -28,5 +20,5 @@ class Payment {
     required this.amount,
     required this.date,
     this.notes,
-  });
+  }) : assert(amount > 0, 'Payment amount must be greater than zero');
 }
